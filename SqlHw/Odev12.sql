3b38739cd762a7144789d83a40bd4726969ce2a1
Ozan Soyak <83428888+ozansyk@users.noreply.github.com>
2021-07-14 20:56:50 +0300

    Create Odev12.sql

---
+++ SqlHw/Odev12.sql
@@ -0,0 +1,29 @@
+SELECT COUNT(*) FROM film
+WHERE length > 
+(
+	SELECT AVG(length) FROM film
+);
+
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = 
+(
+	SELECT MAX(rental_rate) FROM film
+);
+
+
+SELECT * FROM film
+WHERE rental_rate = 
+(
+	SELECT MIN(rental_rate) FROM film
+)
+UNION
+SELECT * FROM film
+WHERE replacement_cost = 
+(
+	SELECT MIN(replacement_cost) FROM film
+);
+
+
+SELECT customer_id, (SELECT COUNT(*) FROM payment WHERE customer_id = customer_id)
+FROM payment;
