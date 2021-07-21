731e5c44d0c3f3c1ca6b584524e74d86f354cda0
Ozan Soyak <83428888+ozansyk@users.noreply.github.com>
2021-07-22 01:59:42 +0300

    Patika hw - Atm Transactions

---
+++ patikaJavaHw/atmTransaction/AtmTransaction.java
@@ -0,0 +1,68 @@
+package patikaJavaHw.atmTransaction;
+
+import java.util.Scanner;
+
+public class AtmTransaction {
+
+	public static void main(String[] args) {
+		String userName, password;
+        Scanner input = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+        while (right > 0) {
+            System.out.print("Kullanýcý Adýnýz :");
+            userName = input.nextLine();
+            System.out.print("Parolanýz : ");
+            password = input.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasýna Hoþgeldiniz!");
+                
+                System.out.println("1-Para yatýrma\n" +
+                        "2-Para Çekme\n" +
+                        "3-Bakiye Sorgula\n" +
+                        "4-Çýkýþ Yap");
+                System.out.print("Lütfen yapmak istediðiniz iþlemi seçiniz : ");
+                select = input.nextInt();
+                while(select!=4) {
+                	switch (select) {
+                		case 1:
+                			System.out.print("Para miktarý : ");
+                			int price = input.nextInt();
+                			balance += price;
+                			break;
+                		case 2:
+                			System.out.print("Para miktarý : ");
+                			price = input.nextInt();
+                			if (price > balance) {
+                				System.out.println("Bakiye yetersiz.");
+                			} else {
+                				balance -= price;
+                			}
+                			break;
+                		case 3:
+                			System.out.println("Bakiyeniz : " + balance);
+                			break;
+                		case 4:
+                			System.out.print("Para miktarý : ");
+                			price = input.nextInt();
+                			balance += price;
+                			break;
+                	}
+                }
+            } else {
+                right--;
+                System.out.println("Hatalý kullanýcý adý veya þifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabýnýz bloke olmuþtur lütfen banka ile iletiþime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkýnýz : " + right);
+                }
+            }
+        
+        }
+        input.close();
+	}
+}
+
