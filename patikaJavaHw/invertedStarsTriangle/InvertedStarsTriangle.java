85addd625a8c80a008c4d0296f9a7c530ea5c3d0
Ozan Soyak <83428888+ozansyk@users.noreply.github.com>
2021-07-22 16:18:25 +0300

    Patika Hw - Inverted Triangle

---
+++ patikaJavaHw/invertedStarsTriangle/InvertedStarsTriangle.java
@@ -0,0 +1,26 @@
+package patikaJavaHw.invertedStarsTriangle;
+
+import java.util.Scanner;
+
+public class InvertedStarsTriangle {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("N sayýsýný giriniz: ");
+		int n = input.nextInt();
+		
+		for(int i=n-1;i>0;i--){
+			for(int j=1;j<=n-i;j++){
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i*2-1;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		input.close();
+	}
+
+}
