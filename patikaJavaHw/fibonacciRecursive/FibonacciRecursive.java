056f83bfe20af2cb47ddef9e09a2feb4597231a6
Ozan Soyak <83428888+ozansyk@users.noreply.github.com>
2021-07-22 22:23:59 +0300

    Patika Hw - Fibonacci Recursive

---
+++ patikaJavaHw/fibonacciRecursive/FibonacciRecursive.java
@@ -0,0 +1,16 @@
+package patikaJavaHw.fibonacciRecursive;
+
+public class FibonacciRecursive {
+
+	public static void main(String[] args) {
+		System.out.println(fibonacci(10));
+	}
+	
+	public static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
