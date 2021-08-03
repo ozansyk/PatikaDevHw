431dbfa2728fa74dd811ba4424ef7f8fefb76c6e
Ozan Soyak <83428888+ozansyk@users.noreply.github.com>
2021-08-03 14:29:05 +0300

    Patika Hw - AdventureGame commit 1

---
+++ patikaJavaHw/adventureGame/Bear.java
@@ -0,0 +1,9 @@
+package patikaJavaHw.adventureGame;
+
+public class Bear extends Obstacle {
+
+	public Bear() {
+		super("Ayý", 7, 20, 12, 2);
+	}
+
+}
---
+++ patikaJavaHw/adventureGame/Cave.java
@@ -0,0 +1,10 @@
+package patikaJavaHw.adventureGame;
+
+
+public class Cave extends BattleLoc{
+
+	Cave(Player player) {
+		super(player, "Maðara", new Zombie(),"Food");
+	}
+
+}
